19025f740bc0d5e542e2466a64d7741f9eead861
Michael Berger <mberger@sipwise.com>
2021-05-04 12:49:41 +0200

    TT#119150 Add journal support to /admins endpoint
    Change-Id: I4635cbb2a44d7e7f7ea31cf72eb0841cc20cf6fb

--- src/modules/admins/admins.controller.ts
+++ src/modules/admins/admins.controller.ts
@@ -1,16 +1,36 @@
-import {Body, Controller, Delete, Get, Param, Post, Put, Query, UseGuards} from "@nestjs/common";
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Inject,
+    Param,
+    Post,
+    Put,
+    Query,
+    UseGuards,
+    UseInterceptors
+} from "@nestjs/common";
 import {ApiCreatedResponse, ApiOkResponse, ApiTags} from "@nestjs/swagger";
 import {AdminsService} from "./admins.service";
 import {CreateAdminDto} from "./dto/create-admin.dto";
 import {UpdateAdminDto} from "./dto/update-admin.dto";
 import {Admin} from "./admin.entity";
 import {OmniGuard} from "../../core/guards/omni.guard";
+import {JournalingInterceptor} from "../../core/interceptors/journaling.interceptor";
+import {JOURNAL_SERVICE} from "../../core/constants";
+import {JournalService} from "../journal/journal.service";
 
 @ApiTags('admins')
 @Controller('admins')
 @UseGuards(OmniGuard)
+@UseInterceptors(JournalingInterceptor)
 export class AdminsController {
-    constructor(private readonly adminsService: AdminsService) {}
+    constructor(
+        private readonly adminsService: AdminsService,
+        // private readonly logger: LoggingService,
+        @Inject(JOURNAL_SERVICE) private readonly journalService: JournalService,
+    ) {}
 
     @Post()
     @ApiCreatedResponse({
@@ -42,5 +62,16 @@ export class AdminsController {
     @Delete(':id')
     async remove(@Param('id') id: string) {
         return await this.adminsService.remove(+id);
+    }
+
+    @Get(':id/journal')
+    async journal(
+        @Param('id') id: string,
+        @Query('page') page: string,
+        @Query('rows') row: string
+    ) {
+        page = page ? page : `${process.env.API_DEFAULT_QUERY_PAGE}`;
+        row = row ? row : `${process.env.API_DEFAULT_QUERY_ROWS}`;
+        return this.journalService.findAll(page, row, 'admins', id)
     }
 }
--- src/modules/admins/admins.module.ts
+++ src/modules/admins/admins.module.ts
@@ -2,10 +2,17 @@ import {Module} from "@nestjs/common";
 import {AdminsController} from "./admins.controller";
 import {adminsProviders} from "./admins.providers";
 import {AdminsService} from "./admins.service";
+import {InterceptorModule} from "../../core/interceptors/interceptor.module";
 
 @Module({
+    imports: [
+        // LoggingService,
+        InterceptorModule],
     controllers: [AdminsController],
     exports: [AdminsService],
-    providers: [AdminsService, ...adminsProviders],
+    providers: [
+        // LoggingService,
+        AdminsService,
+        ...adminsProviders],
 })
 export class AdminsModule {}
