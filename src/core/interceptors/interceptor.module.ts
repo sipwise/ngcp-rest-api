95778b5d055993cc8193ab7a1d50675de3c5f6cf
Michael Berger <mberger@sipwise.com>
2021-05-20 12:25:54 +0200

    TT#116557 Add request logging interceptor
    - logs all HTTP requests for API endpoints that use the interceptor
    - masks sensitive and GDPR complient data
    - masking is achieved by transforming class based data object from request
    to a plain JS object and then transforming it back to the specific class
    - the class used for transforming is determined by the original data constructor value
    
    Change-Id: Id40c328166ea33114b40e7ec86725e70f4b7298a

--- src/core/interceptors/interceptor.module.ts
+++ src/core/interceptors/interceptor.module.ts
@@ -1,10 +1,13 @@
 import {Module} from "@nestjs/common";
 import {JournalingInterceptor} from "./journaling.interceptor";
 import {JournalModule} from "../../modules/journal/journal.module";
+import {LoggingModule} from "../../modules/logging/logging.module";
+import {LoggingService} from "../../modules/logging/logging.service";
+import {LoggingInterceptor} from "./logging.interceptor";
 
 @Module({
-    imports: [JournalModule],
-    providers: [JournalingInterceptor],
-    exports: [JournalModule]
+    imports: [JournalModule, LoggingModule],
+    providers: [JournalingInterceptor, LoggingInterceptor, LoggingService],
+    exports: [JournalModule, LoggingModule],
 })
 export class InterceptorModule {}
