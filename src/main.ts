2dac3ba5584b174a6d0e357ac28fbcbbfac31e61
Michael Berger <mberger@sipwise.com>
2021-05-27 12:30:22 +0200

    TT#116557 Change framework to Express
    - change underlying framework from Fastify to Express
    - current middleware is built on express and is not compatible with
    fastify
    
    Change-Id: Ic2256e1a9795da86faf80b3d4e26ee30863a6d6a

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 
 import * as fs from 'fs';
 import * as dotenv from 'dotenv';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidateInputPipe } from './core/pipes/validate.pipe'
-import { AppClusterService } from './app-cluster.service';
-import { TransformInterceptor } from './core/interceptors/transform.interceptor';
-import {FastifyAdapter} from "@nestjs/platform-fastify";
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
+import {ValidateInputPipe} from './core/pipes/validate.pipe'
+import {AppClusterService} from './app-cluster.service';
+import {TransformInterceptor} from "./core/interceptors/transform.interceptor";
 
 dotenv.config()
+
 
 const httpsOptions = {
     key: fs.readFileSync(process.env.SSL_CERT_KEY_FILE),
@@ -20,25 +20,35 @@ const httpsOptions = {
 }
 
 async function bootstrap() {
-  const app = await NestFactory.create(
-      AppModule,
-      new FastifyAdapter({https: httpsOptions}),
+    // TODO: evaluate and compare default Express vs Fastify
+    //       https://docs.nestjs.com/techniques/performance#adapter
+    const app = await NestFactory.create(
+        AppModule,
+        {
+            httpsOptions: httpsOptions
+        },
+    );
 
-  );
+    const config = new DocumentBuilder()
+        .setTitle("NGCP API")
+        .setDescription("This is the NGCP API description")
+        .setVersion('2.0')
+        .addTag('NGCP')
+        .build();
 
-  // const config = new DocumentBuilder()
-  //   .setTitle("NGCP API")
-  //   .setDescription("This is the NGCP API description")
-  //   .setVersion('2.0')
-  //   .addTag('NGCP')
-  //   .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup(process.env.API_PREFIX, app, document);
 
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup(process.env.API_PREFIX, app, document);
+    app.setGlobalPrefix(process.env.API_PREFIX);
+    app.useGlobalPipes(new ValidateInputPipe())
 
-  app.setGlobalPrefix(process.env.API_PREFIX);
-  app.useGlobalPipes(new ValidateInputPipe())
-  app.useGlobalInterceptors(new TransformInterceptor({pageName: process.env.API_DEFAULT_QUERY_PAGE_NAME, perPageName: process.env.API_DEFAULT_QUERY_ROWS_NAME}))
-  await app.listen(process.env.API_PORT, '0.0.0.0');
+    app.useGlobalInterceptors(
+        new TransformInterceptor({
+            pageName: process.env.API_DEFAULT_QUERY_PAGE_NAME,
+            perPageName: process.env.API_DEFAULT_QUERY_ROWS_NAME
+        })
+    )
+    await app.listen(process.env.API_PORT, '0.0.0.0');
 }
+
 AppClusterService.clusterize(bootstrap);
