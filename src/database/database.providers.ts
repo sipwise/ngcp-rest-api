10d7cdb9315feedb97729c953f586bc1dddb8486
Kirill Solomko <ksolomko@sipwise.com>
2021-06-25 17:13:58 +0200

    TT#126400 disable database models schema sync
    * automatic database schema changes are not allowed,
      therefore sequelize.sync() is disabled
    
    Change-Id: Ie54ec30f982e7ca4c25a7daf47edee92c659e87d
    (cherry picked from commit 980aa9f243cbdf88c165fd69c176ed5966feab2a)

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -25,7 +25,8 @@ export const databaseProviders = [
             config.query = {raw: false}
             const sequelize = new Sequelize(config)
             sequelize.addModels([Admin, Contact, Journal])
-            await sequelize.sync()
+            // models sync is not allowed
+            // await sequelize.sync()
             return sequelize
         },
     },
