a15123ccf5aa078f331185cefb380576ee688ab0
Alexander Lutay <alutay@sipwise.com>
2021-08-17 18:26:38 +0200

    TT#135100 Fix typo in filename /etc/ngcp-rest-api/api.conf
    Otherwise the service doesn't start:
    
    > systemd[1]: Starting NGCP REST API...
    > node[1191962]: internal/fs/utils.js:269
    > node[1191962]:     throw err;
    > node[1191962]:     ^
    > node[1191962]: Error: ENOENT: no such file or directory, open '/etc/ngcp/rest-api/api.conf'
    > node[1191962]:     at Object.openSync (fs.js:462:3)
    > node[1191962]:     at Object.readFileSync (fs.js:364:35)
    > node[1191962]:     at Object.<anonymous> (/usr/share/ngcp-rest-api/server.js:186364:34)
    > node[1191962]:     at __webpack_require__ (/usr/share/ngcp-rest-api/server.js:242749:42)
    > node[1191962]:     at Object.defineProperty.value (/usr/share/ngcp-rest-api/server.js:186320:23)
    > node[1191962]:     at __webpack_require__ (/usr/share/ngcp-rest-api/server.js:242749:42)
    > node[1191962]:     at Object.create.k2 (/usr/share/ngcp-rest-api/server.js:84479:28)
    > node[1191962]:     at __webpack_require__ (/usr/share/ngcp-rest-api/server.js:242749:42)
    > node[1191962]:     at Object.<anonymous> (/usr/share/ngcp-rest-api/server.js:84457:30)
    > node[1191962]:     at __webpack_require__ (/usr/share/ngcp-rest-api/server.js:242749:42) {
    > node[1191962]:   errno: -2,
    > node[1191962]:   syscall: 'open',
    > node[1191962]:   code: 'ENOENT',
    > node[1191962]:   path: '/etc/ngcp/rest-api/api.conf'
    > node[1191962]: }
    > systemd[1]: ngcp-rest-api.service: Main process exited, code=exited, status=1/FAILURE
    > systemd[1]: ngcp-rest-api.service: Failed with result 'exit-code'.
    > systemd[1]: Failed to start NGCP REST API.
    
    Change-Id: I0d1c3ebe38b0144f87357bab46785633bbb7372e

--- src/config/main.config.ts
+++ src/config/main.config.ts
@@ -3,7 +3,7 @@ import {load} from 'js-yaml'
 
 const configFile = process.env.NODE_ENV == 'development'
                         ? './etc/api.conf'
-                        : '/etc/ngcp/rest-api/api.conf'
+                        : '/etc/ngcp-rest-api/api.conf'
 const yaml = load(readFileSync(configFile, 'utf8'))
 
 export const config = yaml
