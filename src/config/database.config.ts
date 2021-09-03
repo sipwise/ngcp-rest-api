22c4c43c68e150c03d72c8d36a4999bf23cb14a2
Kirill Solomko <ksolomko@sipwise.com>
2021-09-03 15:18:19 +0200

    TT#125651 change db entities load mechanism
    * to make webpack happy and not rely on the directory locations,
      the db entities are loaded from a new
      function getDBEntities() that returns an array of Entity objects
    
    Change-Id: I84cbefad30adcf4f7d34e2d25d35d7a7f972e77b

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,10 +1,21 @@
 import {ConnectionOptions} from 'typeorm'
 import {AppService} from '../app.sevice'
+import {db} from '../entities'
 
 const db_user = process.env.API_DB_USER || AppService.config.database.user
 const db_pass = process.env.API_DB_PASS || AppService.config.database.pass
 const db_host = process.env.API_DB_HOST || AppService.config.database.host
 const db_port = process.env.API_DB_PORT || AppService.config.database.port
+
+const getDBEntries = () => {
+    let entities = Array()
+    Object.entries(db).forEach(([,e]) => {
+        Object.entries(e).forEach(([,t]) => {
+            entities.push(t)
+        })
+    })
+    return entities
+}
 
 export const databaseConfig: ConnectionOptions = {
     username: db_user,
@@ -13,8 +24,7 @@ export const databaseConfig: ConnectionOptions = {
     host: db_host,
     type: "mariadb",
     entities: [
-        "dist/entities/db/**/*.js",
-        "prod/entities/db/**/*.js"
+        ...getDBEntries()
     ],
     logging: ["info", "error", "query"]
 }
