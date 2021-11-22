94a8c6b0c478a3263342a5895b4e8356b77d7e18
Kirill Solomko <ksolomko@sipwise.com>
2021-11-22 17:28:10 +0100

    TT#151703 introduce @Public decorator
    * @Public(enabled?: boolean = true) decorator, when applied to a controller or an
      endpoint, disables auth check (basic,jwt,cert,etc.).
      This is useful to for instance open GET /api/fileshare
      to be available for public access
    
    Change-Id: I070e136757933a48bc1a0d647dd94e87ebe8be27
    (cherry picked from commit fb1f8530e502c68b3fd74f50d6c30aedb6c5f1a7)

---
+++ src/guards/public.guard.ts
@@ -0,0 +1,22 @@
+import {CanActivate, ExecutionContext, Injectable} from '@nestjs/common'
+import {Reflector} from '@nestjs/core'
+
+@Injectable()
+export class PublicGuard implements CanActivate {
+    constructor(private reflector: Reflector) {
+    }
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const isPublicMethod = this.reflector.get<boolean>(
+            'isPublic',
+            context.getHandler()
+        )
+        const isPublicController = this.reflector.get<boolean>(
+            'isPublic',
+            context.getClass()
+        )
+
+        return isPublicMethod === undefined ? isPublicController
+                                            : isPublicMethod
+    }
+}
