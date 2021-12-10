b3735a87f196b49952bb8c87c859dda8e54a0438
Kirill Solomko <ksolomko@sipwise.com>
2021-12-10 17:23:19 +0100

    TT#145602 fileshare add size and updated_at
    * size is now present in the response
    * updated_at db field is now filled correctly
    
    Change-Id: Ib0d891a929f4a1cc0970650a404e89dfeb1ca313

--- src/api/fileshare/dto/fileshare-response.dto.ts
+++ src/api/fileshare/dto/fileshare-response.dto.ts
@@ -3,6 +3,7 @@ export class FileshareResponseDto {
     name: string
     mime_type: string
     ttl: number
+    size: number
     created_at: Date
     expires_at: Date
     subscriber_id?: number
