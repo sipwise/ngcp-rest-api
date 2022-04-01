711745a8f27c3595c03c70ca5da38f53f45e7d65
Kirill Solomko <ksolomko@sipwise.com>
2022-04-01 15:51:49 +0200

    TT#169150 fix pbxgroups-response swagger circular dependency
    * add PbxgroupMember class to pbxgroups-response and use
      it in the property definition instead of the anonymous
      object stuct
    * add ApiProperty({ type: PbxgroupMember }) so that swagger
      use it correctly instead of trying to build using own guessing
      causing it to run into a ciclular dependency issue
    
    Change-Id: Ife4047feb709f7571e23b3c1d51d9af68518dba9

--- src/api/pbxgroups/dto/pbxgroups-response.dto.ts
+++ src/api/pbxgroups/dto/pbxgroups-response.dto.ts
@@ -1,11 +1,20 @@
+import {ApiProperty} from '@nestjs/swagger'
 import {internal} from '../../../entities'
+
+export class PbxgroupMember {
+    extension: string
+    subscriber_id: number
+}
 
 export class PbxgroupsResponseDto {
     extension: string
     hunt_policy: string
     hunt_timeout: number
     subscriber_id: number
-    members: {extension: string, subscriber_id: number}[]
+    @ApiProperty({
+        type: PbxgroupMember
+    })
+    members: PbxgroupMember[]
     name: string
 
     constructor(pbxGroup: internal.PbxGroup) {
