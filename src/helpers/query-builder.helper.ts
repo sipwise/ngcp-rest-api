2e69527e784454e26fc7b0971c95ae5108477713
Michael Berger <mberger@sipwise.com>
2022-04-06 09:27:07 +0200

    TT#170200 implement wildcard search
    - allow wildcard search using '*' in params
    - add typing to SearchLogic interface
    - add typing to QueryBuilder helper functions
    - refactor configureQueryBuilder for better code quality
    
    Change-Id: Ie1ac3b63bd7d95e0739cb0f01edf107c4a02ffc7

--- src/helpers/query-builder.helper.ts
+++ src/helpers/query-builder.helper.ts
@@ -1,42 +1,61 @@
-import {SelectQueryBuilder} from 'typeorm'
+import {BaseEntity, SelectQueryBuilder} from 'typeorm'
 
 export interface SearchLogic {
     joins?: {
         alias: string,
         property: string
     }[],
-    where: {},
+    where: string[],
     rows?: number,
     page?: number
 }
 
-export async function configureQueryBuilder(queryBuilder: SelectQueryBuilder<any>, requestParams: string[], searchLogic: SearchLogic) {
+export async function configureQueryBuilder<T extends BaseEntity>(qb: SelectQueryBuilder<T>, params: string[], searchLogic: SearchLogic) {
+    await addJoinFilterToQueryBuilder(qb, params, searchLogic)
+    await addSearchFilterToQueryBuilder(qb, params, searchLogic)
+    await addPaginationToQueryBuilder(qb, searchLogic)
+    qb.andWhere('1 = 1')
+}
+
+async function addJoinFilterToQueryBuilder<T extends BaseEntity>(qb: SelectQueryBuilder<T>, params: string[], searchLogic: SearchLogic) {
     for (const joinCondition in searchLogic.joins) {
         const joinTable = searchLogic.joins[joinCondition].alias
         const joinColumn = searchLogic.joins[joinCondition].property
         if (joinTable != undefined && joinColumn != undefined) {
-            queryBuilder.leftJoinAndSelect(`${queryBuilder.alias}.${joinColumn}`, `${joinColumn}`)
-            if (requestParams[joinColumn] != null) {
-                queryBuilder.where(`${joinTable}.${joinColumn} = :${joinColumn}`, {[`${joinColumn}`]: requestParams[joinColumn]})
+            qb.leftJoinAndSelect(`${qb.alias}.${joinColumn}`, `${joinColumn}`)
+            if (params[joinColumn] != null) {
+                qb.where(`${joinTable}.${joinColumn} = :${joinColumn}`, {[`${joinColumn}`]: params[joinColumn]})
             }
         }
     }
-    for (const paramIndex in searchLogic.where) {
-        const paramName = searchLogic.where[paramIndex]
+}
+
+async function addSearchFilterToQueryBuilder<T extends BaseEntity>(qb: SelectQueryBuilder<T>, params: string[], searchLogic: SearchLogic) {
+    for (const property of searchLogic.where) {
         //if a JOIN has happened based on this request parameter, skip it for the WHERE clause
-        if (searchLogic.joins?.some(j => j.property === paramName)) {
+        if (searchLogic.joins?.some(j => j.property === property)) {
             continue
         }
-        if (requestParams[paramName] != null)
-            queryBuilder = (requestParams['search_or'] === '1') ?
-                queryBuilder.orWhere(`${queryBuilder.alias}.${paramName} = :${paramName}`, {[`${paramName}`]: requestParams[paramName]}) :
-                queryBuilder.andWhere(`${queryBuilder.alias}.${paramName} = :${paramName}`, {[`${paramName}`]: requestParams[paramName]})
+        if (params[property] != null) {
+            let value: string = params[property]
+
+            const whereComparator = value.includes('*')  ? 'like' : '='
+            value = value.replace(/\*/g, '%')
+
+            if (params['search_or'] === '1') {
+                qb.orWhere(`${qb.alias}.${property} ${whereComparator} :${property}`, {[`${property}`]: value})
+            } else {
+                qb.andWhere(`${qb.alias}.${property} ${whereComparator} :${property}`, {[`${property}`]: value})
+            }
+        }
     }
+}
+
+async function addPaginationToQueryBuilder<T extends BaseEntity>(qb: SelectQueryBuilder<T>, searchLogic: SearchLogic) {
     if (searchLogic.rows != null) {
-        queryBuilder.limit(searchLogic.rows)
+        qb.limit(searchLogic.rows)
     }
     if (searchLogic.rows != null && searchLogic.page != null) {
-        queryBuilder.offset(searchLogic.rows * (searchLogic.page - 1))
+        qb.offset(searchLogic.rows * (searchLogic.page - 1))
     }
-    queryBuilder.andWhere('1 = 1')
 }
