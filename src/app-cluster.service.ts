81a9e6d003d56de9a65be9fc60e4743b14f35b4c
Kirill Solomko <ksolomko@sipwise.com>
2022-10-06 10:03:09 +0200

    MT#55560 improve startup logging handling in app-cluster.service
    * the startup code is now inside the try/catch block to correctly
      log the produced error via the Logger so that it has the correct
      process name and correctly caught by by rsyslog for redirection
      to the rest-api.log
    * NODE_CONSOLE env is now considered (boolean) and if specified
      it forces the logging to the console
    * change behaviour of the default logging output, now when
      the syslog output is used only if NODE_ENV=='production' and
      NODE_CONSOLE is not true
    
    Change-Id: Ic183c4b52ec185f359cb253482af55cbf281786b

--- src/app-cluster.service.ts
+++ src/app-cluster.service.ts
@@ -3,8 +3,10 @@ import cluster from 'cluster'
 import fs from 'fs'
 import sdNotify from 'sd-notify'
 import {AppService} from './app.service'
-import {WinstonLogger, WinstonModule} from 'nest-winston'
+import {WinstonModule} from 'nest-winston'
 import {winstonLoggerConfig} from './config/logger.config'
+import {exit} from 'process'
+import {LoggerService} from './logger/logger.service'
 
 const workersAmount = AppService.config.common.workers
 
@@ -19,53 +21,62 @@ let workersOnline = 0
 export class AppClusterService {
     // eslint-disable-next-line @typescript-eslint/ban-types
     static clusterize(callback: Function): void {
-        const logger = WinstonModule.createLogger(winstonLoggerConfig)
-        if (cluster.isWorker) {
-            logger.log(`Cluster worker PID: ${process.pid}`)
-            callback()
-        } else {
-            logger.log(`Master server started with PID: ${process.pid} Workers: ${workersAmount}`)
-            for (let i = 0; i < workersAmount; i++) {
-                cluster.fork()
+        let logger: LoggerService
+        try {
+            logger = WinstonModule.createLogger(winstonLoggerConfig) as LoggerService
+            if (cluster.isWorker) {
+                logger.log(`Cluster worker PID: ${process.pid}`)
+                callback()
+            } else {
+                logger.log(`Master server started with PID: ${process.pid} Workers: ${workersAmount}`)
+                for (let i = 0; i < workersAmount; i++) {
+                    cluster.fork()
+                }
+                cluster.on('exit', (worker, code, signal) => {
+                    logger.log(`Worker ${worker.process.pid} died (${code} ${signal})`)
+                    if (workersOnline > 0) {
+                        workersOnline -= 1
+                    }
+                    if (!workersOnline) {
+                        sdNotify.sendStatus('STOPPING=1')
+                        if (fs.existsSync(`${pidDir}/${pidFile}`)) {
+                            fs.unlinkSync(`${pidDir}/${pidFile}`)
+                        }
+                        logger.log('Server is stopped')
+                        started = 0
+                    }
+                })
+                cluster.on('disconnect', (worker) => {
+                    if (started) {
+                        sdNotify.sendStatus('STOPPING=1')
+                        if (fs.existsSync(`${pidDir}/${pidFile}`)) {
+                            fs.unlinkSync(`${pidDir}/${pidFile}`)
+                        }
+                        logger.log('Server is stopped')
+                        started = 0
+                    }
+                })
+                cluster.on('listening', (worker) => {
+                    logger.log(`Worker ${worker.process.pid} is ready`)
+                    workersOnline += 1
+                    if (!started && workersOnline == workersAmount) {
+                        if (!fs.existsSync(pidDir)) {
+                            fs.mkdirSync(pidDir, {recursive: true})
+                        }
+                        fs.writeFileSync(`${pidDir}/${pidFile}`, `${process.pid}\n`)
+                        logger.log('Server is started')
+                        sdNotify.sendStatus('READY=1')
+                        sdNotify.ready()
+                        started = 1
+                    }
+                })
             }
-            cluster.on('exit', (worker, code, signal) => {
-                logger.log(`Worker ${worker.process.pid} died (${code} ${signal})`)
-                if (workersOnline > 0) {
-                    workersOnline -= 1
-                }
-                if (!workersOnline) {
-                    sdNotify.sendStatus('STOPPING=1')
-                    if (fs.existsSync(`${pidDir}/${pidFile}`)) {
-                        fs.unlinkSync(`${pidDir}/${pidFile}`)
-                    }
-                    logger.log('Server is stopped')
-                    started = 0
-                }
-            })
-            cluster.on('disconnect', (worker) => {
-                if (started) {
-                    sdNotify.sendStatus('STOPPING=1')
-                    if (fs.existsSync(`${pidDir}/${pidFile}`)) {
-                        fs.unlinkSync(`${pidDir}/${pidFile}`)
-                    }
-                    logger.log('Server is stopped')
-                    started = 0
-                }
-            })
-            cluster.on('listening', (worker) => {
-                logger.log(`Worker ${worker.process.pid} is ready`)
-                workersOnline += 1
-                if (!started && workersOnline == workersAmount) {
-                    if (!fs.existsSync(pidDir)) {
-                        fs.mkdirSync(pidDir, {recursive: true})
-                    }
-                    fs.writeFileSync(`${pidDir}/${pidFile}`, `${process.pid}\n`)
-                    logger.log('Server is started')
-                    sdNotify.sendStatus('READY=1')
-                    sdNotify.ready()
-                    started = 1
-                }
-            })
+        }
+        catch(e) {
+            logger ? logger.error(e) : console.error(e)
+            sdNotify.sendStatus('STOPPING=1')
+            started = 0
+            exit(1)
         }
     }
 }
