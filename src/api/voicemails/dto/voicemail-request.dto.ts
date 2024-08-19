18125940246da743bf9d5b64405d449340782f1e
Kirill Solomko <ksolomko@sipwise.com>
2024-08-19 19:19:14 +0200

    MT#60720 voicemails fix search by time column
    * search by time is now correctly aliased to the internal
      origtime
    
    Change-Id: I82ceec4c0d2f1e3109d912c2aeb4efdcba958af3
    (cherry picked from commit 99254785c291202f4677a2cfce6692444ddc678d)
    (cherry picked from commit 2550ac4b3dc48aec6169ce08cac14e7afbbbb5da)

--- src/api/voicemails/dto/voicemail-request.dto.ts
+++ src/api/voicemails/dto/voicemail-request.dto.ts
@@ -6,16 +6,6 @@ export class VoicemailRequestDto implements RequestDto {
     @ApiProperty({description: 'message folder', example: ['Old', 'Inbox']})
         folder: string
 
-    constructor(entity?: internal.Voicemail) {
-        if (!entity)
-            return
-
-        // TODO rework as the Dto key names are not always equal to the Entity ones
-        Object.keys(entity).map(key => {
-            this[key] = entity[key]
-        })
-    }
-
     toInternal(options: RequestDtoOptions = {}): internal.Voicemail {
         const voicemail = new internal.Voicemail()
 
