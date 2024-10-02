5fdfcfa849ef1509afb128e0e038c831fb15be94
Bertold Vincze <bvincze@sipwise.com>
2024-10-02 12:23:12 +0200

    MT#60503 Fix license handling
    * In case where the procdirectory with the licenses are not found consider all licenses to fail
    
    Change-Id: Ia337dc8fcd4b82b3c57cb3e8465fccfb06cbb367

--- src/repositories/license.repository.ts
+++ src/repositories/license.repository.ts
@@ -9,25 +9,28 @@ export class LicenseRepository {
     private directoryPath = procLicensesLocation
 
     async readLicenses(): Promise<License> {
-        const files = await fs.readdir(this.directoryPath)
         const licenseStatuses = {} as License
-
-        for (const file of files) {
-            const filePath = path.join(this.directoryPath, file)
-            const data = await fs.readFile(filePath, 'utf8')
-            const isEnabled = data.trim() === '1' ? 1 : 0
-
-            if (Object.values(LicenseType).includes(file as LicenseType)) {
-                licenseStatuses[file as LicenseType] = isEnabled
-            }
-        }
-
         for (const key of Object.values(LicenseType)) {
-            if (!(key in licenseStatuses)) {
-                licenseStatuses[key] = 0
-            }
+            licenseStatuses[key] = 0
         }
 
-        return licenseStatuses
+        try {
+            const files = await fs.readdir(this.directoryPath)
+            for (const file of files) {
+                const filePath = path.join(this.directoryPath, file)
+                try {
+                    const data = await fs.readFile(filePath, 'utf8')
+                    const isEnabled = data.trim() === '1' ? 1 : 0
+                    if (Object.values(LicenseType).includes(file as LicenseType)) {
+                        licenseStatuses[file as LicenseType] = isEnabled
+                    }
+                } catch {
+                    continue
+                }
+            }
+            return licenseStatuses
+        } catch {
+            return licenseStatuses
+        }
     }
 }
