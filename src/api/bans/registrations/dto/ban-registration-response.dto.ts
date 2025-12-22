d5201f116f0984c81087435e674b4eb727ef6901
Kirill Solomko <ksolomko@sipwise.com>
2025-12-22 14:19:16 +0100

    MT#63959 adjust /api/bans/registrations /api/bans/ips
    * /api/bans/registrations:
      - add 2 new response fields: auth_count (number), last_auth (Date)
    * /api/bans/ips:
      - fix multiple entries to be returned properly
    
    Change-Id: Ib7598269ae3fca92ed6061bc3b04109d4f4c00cb
    (cherry picked from commit aaa97d43f7c01dee00c2235e59d89107f62e2f6b)
    (cherry picked from commit 8192cbdcd1b0a45fc49d433e949db0d54a0a1368)

--- src/api/bans/registrations/dto/ban-registration-response.dto.ts
+++ src/api/bans/registrations/dto/ban-registration-response.dto.ts
@@ -14,10 +14,18 @@ export class BanRegistrationResponseDto extends ResponseDto {
     @ApiProperty()
         domain!: string
 
+    @ApiProperty()
+        auth_count!: number
+
+    @ApiProperty()
+        last_auth!: Date
+
     constructor(entity: internal.BanRegistration, options?: ResponseDtoOptions) {
         super(options)
         this.id = entity.id
         this.username = entity.username
         this.domain = entity.domain
+        this.auth_count = entity.authCount
+        this.last_auth = entity.lastAuth
     }
 }
