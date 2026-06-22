bce4e1fdf42c01f6d75823ef1465460ad247e070
Kirill Solomko <ksolomko@sipwise.com>
2026-06-22 18:55:54 +0200

    MT#65347 task agent and redis connection improvements
    * remove @nestjs-modules/ioredis options and
      createRedisConnection() usage to use pure ioredis
      as providers are either way reused and @nestjs-modules/ioredis
      main use for forRoot() functionality.
    * rework redis.config for ioredis.
    * upgrade ioredis to 5.4.1 to 5.11.1
    * disable ioredis keepAlive and use own keepalive
      via pings in redisstate.schedule as ioredis keepalive
      functionality conflicts with haproxy it seems causing
      lost connections and periodic reconnects.
    * rework checkRedisAvailable() to check both redis
      and redisPubSub connections.
    * task-agent.helper
        - rework data accumulation to retrieve multiple
          responses from task-agent(s)
        - rework timeouts logic and data flow per replied
          task-agent host
        - improve logic for when consider the task is completed
          (all possible responses are recieved)
        - improve logging
    
    Change-Id: I88af5d1619c8b161e92825d5a84e15aff299d31f
    (cherry picked from commit e67a5f4bcdf7ab704c188b0db5d1a48e7c9da7cd)
    (cherry picked from commit 76a0eb787a11219cb831d56ec20acf4f0053ebbf)

--- src/database/redis.providers.ts
+++ src/database/redis.providers.ts
@@ -1,5 +1,4 @@
-import {createRedisConnection} from '@nestjs-modules/ioredis'
-import {Cluster,Redis} from 'ioredis'
+import {Redis} from 'ioredis'
 
 import {redisConfig} from '~/config/redis.config'
 import {LoggerService} from '~/logger/logger.service'
@@ -7,15 +6,19 @@ import {LoggerService} from '~/logger/logger.service'
 export const redisProviders = [
     {
         provide: 'Redis',
-        useFactory: async (): Promise<Redis | Cluster> => {
-            let redis: Redis | Cluster
+        useFactory: async (): Promise<Redis> => {
+            let redis: Redis
             const log = new LoggerService('databaseProviders[Redis]')
             if (process.env.NODE_ENV == 'test' && process.env.NODE_TEST_E2E !== 'true') {
                 log.debug('test environment detected, skip database connection')
                 return redis
             }
             try {
-                redis  = createRedisConnection(redisConfig)
+                redis = new Redis(redisConfig)
+                redis.on('error', function(err) {
+                    log.error('Redis error:', err)
+                })
+                await redis.connect()
                 log.debug('Connected to Redis')
             } catch (err) {
                 log.error(`Could not connect to Redis: ${err}`)
@@ -25,15 +28,19 @@ export const redisProviders = [
     },
     {
         provide: 'RedisPubSub',
-        useFactory: async (): Promise<Redis | Cluster> => {
-            let redis: Redis | Cluster
+        useFactory: async (): Promise<Redis> => {
+            let redis: Redis
             const log = new LoggerService('databaseProviders[RedisPubSub]')
             if (process.env.NODE_ENV == 'test' && process.env.NODE_TEST_E2E !== 'true') {
                 log.debug('test environment detected, skip database connection')
                 return redis
             }
             try {
-                redis  = createRedisConnection(redisConfig)
+                redis = new Redis(redisConfig)
+                redis.on('error', function(err) {
+                    log.error('Redis (PubSub) error:', err)
+                })
+                await redis.connect()
                 log.debug('Connected to Redis (PubSub)')
             } catch (err) {
                 log.error(`Could not connect to Redis (PubSub): ${err}`)
