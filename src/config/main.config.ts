77c284a3fe6ce7a6456b81aeb98c71ffb8537f8e
Kirill Solomko <ksolomko@sipwise.com>
2026-08-24 12:26:39 +0200

    MT#65820 add api.conf file validation on startup
    * loadConfig() fails with 'malformed api.conf' if the
      yaml content cannot be loaded or 'invalid api.conf'
      if the config cannot be properly validated.
    
    Change-Id: Id15a1e23c72a186a5b5669197021a1045fd01a62
    (cherry picked from commit 1af93d0f67e3215c5c16a9143ae1d8374820364c)

--- src/config/main.config.ts
+++ src/config/main.config.ts
@@ -1,22 +1,40 @@
 import {readFileSync} from 'fs'
+import {basename} from 'path'
+import {exit} from 'process'
 
 import {plainToInstance} from 'class-transformer'
-import {validateOrReject} from 'class-validator'
+import {validateSync} from 'class-validator'
 import {load} from 'js-yaml'
 
 import {AppConfig} from './schemas/app.config.schema'
+
+import {LoggerService} from '~/logger/logger.service'
 
 const configFile = process.env.NODE_ENV == 'development' || process.env.NODE_ENV == 'test'
     ? './etc/api.conf'
     : '/etc/ngcp-rest-api/api.conf'
 
-const yaml = load(readFileSync(configFile, 'utf8'))
+function loadConfig(): AppConfig {
+    let yaml: unknown
+    try {
+        yaml = load(readFileSync(configFile, 'utf8'))
+    } catch {
+        const fileName = basename(configFile)
+        new LoggerService('config')
+            .error(`Cannot start the server, malformed configuration file '${fileName}'.`)
+        exit(1)
+    }
 
-const configInstance = plainToInstance(AppConfig, yaml)
+    const instance = plainToInstance(AppConfig, yaml)
+    const errors = validateSync(instance, {forbidUnknownValues: false})
+    if (errors.length > 0) {
+        const fileName = basename(configFile)
+        new LoggerService('config')
+            .error(`Cannot start the server, invalid configuration file '${fileName}'.`)
+        exit(1)
+    }
 
-validateOrReject(configInstance)
-    .catch(errors => {
-        throw new Error(`Configuration validation error: ${JSON.stringify(errors)}`)
-    })
+    return instance
+}
 
-export const config = configInstance
+export const config = loadConfig()
